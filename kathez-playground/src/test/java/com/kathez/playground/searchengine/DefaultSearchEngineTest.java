b5efa85eabbd7b4f500d50c63c563269b01a50b5
Balaji <hr@fiotechnologies.in>
2014-06-02 23:49:27 +0100

    Initial test case

---
+++ kathez-playground/src/test/java/com/kathez/playground/searchengine/DefaultSearchEngineTest.java
@@ -0,0 +1,16 @@
+package com.kathez.playground.searchengine;
+
+import org.junit.Test;
+
+/**
+ * @author Balaji
+ *
+ */
+public class DefaultSearchEngineTest {
+	
+	@Test
+	public void testSearch(){
+		
+	}
+
+}
